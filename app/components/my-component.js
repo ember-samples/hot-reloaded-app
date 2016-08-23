61059bbd108a18b1013b29eb0d33ec259e9ad859
Miguel Madero <git@miguelmadero.com>
2016-08-23 10:13:46 -0300

    Switches to use an inline templatE

--- app/components/my-component.js
+++ app/components/my-component.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  prop1: 'Hi2'
+  layout: Ember.HTMLBars.compile(`
+    From template 3<br>
+    From JS: {{prop1}}
+    {{yield}}
+  `),
+  prop1: 'Hi3'
 });
